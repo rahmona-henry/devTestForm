e3d5078c3ab00ba6cc1df4f30ad6bcd6112176e9
rahmona-henry <rhenry@hubspot.com>
2020-11-05 10:34:39 +0000

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(express.static('index.html'))
+app.use(express.static('client'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','hbs')
