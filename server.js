465de7d6efd449beb732a7a79d00e06b8e41064b
rahmona-henry <rhenry@hubspot.com>
2020-11-05 10:29:56 +0000

    test

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(express.static('index.html'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine','hbs')
+
+
+
+///Get Routes/////
+app.get('/', function (req,res) {
+  res.send(index.html)
+})
+
+///Post Routes/////
+app.post('https://forms.hubspot.com/uploads/form/v2/4014783/b5bd028f-9547-40e0-af6e-95fae78fa1bf', function(req,res){
+    res.send(thankYou.html)
+     })
+
+///Listen Route///////
+
+app.listen(3000, function(){
+  console.log('We have lift off on port 3000')
+})
